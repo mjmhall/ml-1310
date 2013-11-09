59048496afe87ad7d22d246f28751632f8c6158b
Martin Hall <mjm.hall@gmail.com>
2013-11-09 00:12:43 +0000

    homework upto option 3

--- mlclass-ex2/costFunction.m
+++ mlclass-ex2/costFunction.m
@@ -21,12 +21,9 @@ grad = zeros(size(theta));
 %
 
 
+J = 1/m * sum(-y .* log(sigmoid(X * theta)) - (1 - y) .* log(1 - sigmoid(X * theta)))
 
 
-
-
-
-
+grad = (1/m * (sigmoid(X * theta) - y)' * X)';
 % =============================================================
-
 end
--- mlclass-ex2/plotData.m
+++ mlclass-ex2/plotData.m
@@ -5,7 +5,18 @@ function plotData(X, y)
 
 % Create New Figure
 figure; hold on;
+ 
+ _x = [X,y];
+%Admitted = _x(find(_x(:,3)==1),[1,2]);
+%Not_Admitted = _x(find(_x(:,3)==0),[1,2]);
 
+%scatter(Admitted(:,1),Admitted(:,2),"black","k+","filled","MarkerSize",7);
+%scatter(Not_Admitted(:,1),Not_Admitted(:,2),'ko',MarkerFaceColor','y','MarkerSize',7);
+
+pos = find(y==1); neg = find(y == 0);
+% Plot Examples
+plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2,'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
 % ====================== YOUR CODE HERE ======================
 % Instructions: Plot the positive and negative examples on a
 %               2D plot, using the option 'k+' for the positive
